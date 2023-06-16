f5dca8b7c2118e3e6eb361d602c48245aabc7807
ruslanonly <rzaevruslanonly@gmail.com>
2023-06-16 19:17:43 +0300

    Set up project.

---
+++ src/js/main/index.js
@@ -0,0 +1,2 @@
+import "../../styles/main/main.scss"
+
---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlNames = ['main'];
+
+const htmlPlugins = htmlNames.map((fileName) => {
+  return new HtmlWebpackPlugin({
+    template: `src/html/${fileName}.html`,
+    filename: `${fileName}/${fileName}.html`,
+    chunks: [fileName],
+  });
+});
+
+module.exports = {
+  entry: {
+    main: './src/js/main/index.js',
+  },
+  output: {
+    filename: '[name]/[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },  
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              sources: {
+                list: [
+                  {
+                    tag: 'img',
+                    attribute: 'src',
+                    type: 'src',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]/[name].bundle.css',
+      chunkFilename: '[id].css',
+    }),
+    ...htmlPlugins,
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: 'common',
+    },
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'build'),
+    },
+    port: 3000,
+  },
+};
